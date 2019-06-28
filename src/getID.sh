5cf30a3cdad38f0c772c57d041d8ff8f06b1979c
Gavin Day <gday1992@gmail.com>
2019-06-28 17:54:32 +0100

    changed the download file ++ speed improvements

--- src/getID.sh
+++ src/getID.sh
@@ -1,11 +1,7 @@
 #pwd
 #rm temp/channelXMLList.conf
 input="channelUrlList.conf"
-while IFS= read -r line
-do
-    #echo $line
-    ID=$(wget -q -O - $line | grep '"channelId"' | cut -d\" -f4)
-    echo "https://www.youtube.com/feeds/videos.xml?channel_id=$ID" >> temp/channelXMLList.conf
-    
+url="https://www.youtube.com/feeds/videos.xml?channel_id="
+cat $input | xargs -n 1 -P 100 -I % sh -c ' echo 'https://www.youtube.com/feeds/videos.xml?channel_id='"$(wget % -q -O - | grep '"channelId"' | cut -d\" -f4 | grep -v '"c4-flexible-player-box"')" >> temp/channelXMLList.conf'
 
-done < "$input"
+
